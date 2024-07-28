839cbfb1ae421c7d10c22818196aaea838909b8e
Tauseef Ahmad <tauseefahmad.se@gmail.com>
2024-07-28 15:48:17 +0500

    React

--- Day-39/eccommerce/backend/config/db.js
+++ Day-39/eccommerce/backend/config/db.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 //const { MONGODB_URL } = require('./config');
 
 const dbConnect = () => {
-  mongoose.connect('mongodb://localhost:27017/demo_db')
+  //working ok
+  //mongoose.connect('mongodb+srv://tauseef:12345@cluster0.ntlf1bx.mongodb.net/')
+
+  //not working as localhost
+   //mongoose.connect('mongodb://localhost:27017/')
+  
+   mongoose.connect('mongodb://127.0.0.1:27017/')
   .then(conn => console.log("db is connected"))
   .catch(err => console.log("db connection failed"))
 }
